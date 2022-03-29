023ec68df62a7a6494f1a67d7f07f7f83f042b41
unknown <sezeranochrisostom123@gmail.com>
2022-03-29 12:44:17 +0200

    ch(dotenv) dotenv set up
    Adds babel compiler
    adds dotenv configuration
    adds env valiables
    
    Finish #TP-40

---
+++ app.js
@@ -0,0 +1,8 @@
+import dotEnv from 'dotenv';
+
+/* ========== setting up dotenv ============= */
+dotEnv.config()
+
+// accessing dotEnv variable
+console.log(process.env.ENVRONOMENT)
+/* ========== setting up dotenv ============= */
