43d087b72a0cb138f8346191f5967eb2e00ecce9
chanceDira <chancedesire60@gmail.com>
2022-03-30 13:02:24 +0200

    ch(Swagger): worked on feedback
    Corrected swagger
    Edit README file
    
    DONE #TP-23

--- routes/users/users.js
+++ routes/users/users.js
@@ -32,51 +32,8 @@ const router = express.Router();
  *        
  */
 
-/**
- * @swagger
- * /users:
- *   get:
- *     summary: Returns all users
- *     tags: [Users]
- *     responses:
- *       200:
- *         description: The list of the users
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- */
 router.get('/', userController.getUsers);
 
-/**
- * @swagger
- * /users:
- *   post:
- *     summary: Create a new user
- *     tags: [Users]
- *     parameters:
- *      - in: header
- *        name: token
- *        schema:
- *          type: string
- *        required: true
- *        description: Auth-token 
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Dashboard'
- *     responses:
- *       200:
- *         description: The user was successfully created
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Dashboard'
- *       500:
- *         description: Some server error
- */
 router.post('/', userController.postUser);
 
 export default router;
