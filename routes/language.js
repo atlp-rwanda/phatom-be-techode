5df2c511cb39ede3199f628addb05c38d7639825
wakaflorien <waka.florien45@gmail.com>
2022-03-31 16:01:12 +0200

    ch(translation) translates and returns data both in english and fresh up selected language WIP #TP-20

--- routes/language.js
+++ routes/language.js
@@ -5,4 +5,8 @@ router.get('/welcome', (req,res) => {
     res.status(200).json({ "status": `${req.t('success')}`, "data": `${req.t('welcome')}`})
 })
 
+<<<<<<< HEAD
 export default router
+=======
+module.exports = router
+>>>>>>> ch(translation) translates and returns data both in english and fresh up selected language
