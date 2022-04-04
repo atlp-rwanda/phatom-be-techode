aaf1be5c791da1a966ae5e8ce070e4ea8f00832b
unknown <sezeranochrisostom123@gmail.com>
2022-04-04 18:00:07 +0200

    ch(Postgres) postgres setup
    sequelize
    migration script
    
    DONE #TP-17

--- config/config.js
+++ config/config.js
@@ -12,15 +12,23 @@ module.exports = {
     url: process.env.DATABASE_PRODUCTION,
     dialect: 'postgres',
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> ch(Postgres) postgres setup
     logging:false,
     ssl: false,
     dialectOptions: {
       ssl: { rejectUnauthorized: false },
     }
+<<<<<<< HEAD
   },
 }
 =======
   },
 }
 
+>>>>>>> ch(Postgres) postgres setup
+=======
+  },
+}
 >>>>>>> ch(Postgres) postgres setup
